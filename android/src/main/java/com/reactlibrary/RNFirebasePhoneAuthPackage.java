cd46842f0f03a7de2ead2cb67c3ac23ac6429714
Lion Burger <lionb@welldone-software.com>
2017-08-23 11:13:42 +0300

    RN v0.47 compatibility

--- android/src/main/java/com/reactlibrary/RNFirebasePhoneAuthPackage.java
+++ android/src/main/java/com/reactlibrary/RNFirebasePhoneAuthPackage.java
@@ -16,7 +16,7 @@ public class RNFirebasePhoneAuthPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNFirebasePhoneAuthModule(reactContext));
     }
 
-    @Override
+//    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
